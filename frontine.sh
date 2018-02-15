dc7c711152a24b61319dfb22298dec01326c0110
Jeremy DeLong <jeremy@messageagency.com>
2018-02-15 03:19:13 -0500

    falling asleep at keyboard...  good times...

--- frontine.sh
+++ frontine.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##############
-# This script is my answer to the code challenge I was given.  
+# This script is my answer to the code challenge I was given.
 # Convert the string:
 # "(id,created,employee(id,firstname,employeeType(id), lastname),location)"
 # to the following output
@@ -37,20 +37,42 @@ THESTRING=$(echo ${THESTRING} | sed 's/(/ /g')
 THESTRING=$(echo ${THESTRING} | sed 's/)/ /g')
 THESTRING=$(echo ${THESTRING} | sed 's/,/ /g')
 
-# Step 2.  Let's put the string bits into an array.  If I were teaching a jr.
+# Step 2.  Let's put the stringy bits into an array.  If I were teaching a jr.
 # dev, I would want to teach them to anticipate what might be coming down the
 # pike, and having the data in an array that can be easily accessed would be an
 # easy win.
 declare -a THEARRAY
+declare -a THENEWARRAY
+
 for l in $THESTRING; do
-       THEARRAY+=($l)
+    THEARRAY+=(${l})
 done
 
-## Saved for later
+# TODO: These should be split into three diff arrays, that way they could each
+# be sorted separately, and then reassembled in correct order.
+for l in ${!THEARRAY[@]}; do
+  if [ $l = 6 ]; then
+    THENEWARRAY+=(" -- ${THEARRAY[$l]}")
+  elif [ $l = 3 ] || [ $l = 4 ] || [ $l = 5 ] || [ $l = 7 ]; then
+    THENEWARRAY+=(" - ${THEARRAY[$l]}")
+  else
+    THENEWARRAY+=(${THEARRAY[$l]})
+  fi
+done
 
-for k in "${!THEARRAY[@]}"
+
+for k in "${!THENEWARRAY[@]}"
 do
-    echo $k ' - ' ${THEARRAY["$k"]}
+    echo ${THENEWARRAY["$k"]}
 done
-# |
-# sort -rn -k3
+
+######
+#This works, and gives the expected info.  It is ineligent though, and I should
+#have time tomorrow to clean it up before I have to turn it in.  I think that
+#creating three arrays is still the better way to do it, since then I can sort
+#them before reassembly.
+
+
+
+# This would be how I could handle the sort tomorrow...
+#| sort -k3
