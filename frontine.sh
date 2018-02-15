e768eed9595e4b373faf6cca5791267b32ac3742
Jeremy DeLong <jeremy@messageagency.com>
2018-02-15 01:29:12 -0500

    initial commit - power just flickered from heavy rain

---
+++ frontine.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+
+# Convert the string:
+THESTRING="(id,created,employee(id,firstname,employeeType(id), lastname),location)"
+# to the following output
+#
+# id
+# created
+# employee
+# - id
+# - firstname
+# - employeeType
+# -- id
+# - lastname
+# location
+#
+# Bonus (output in alphabetical order):
+# created
+# employee
+# - employeeType
+# -- id
+# - firstname
+# - id
+# - lastname
+# id
+# location
+
+
+# Step 1:  Let's split the string into space delimited pieces.
+#
+THESTRING=$(echo ${THESTRING} | sed 's/(/ /g')
+THESTRING=$(echo ${THESTRING} | sed 's/)/ /g')
+THESTRING=$(echo ${THESTRING} | sed 's/,/ /g')
+
+# Step 2.  Let's put the string bits into an array.  If I were teaching a jr.
+# dev, I would want to teach them to anticipate what might be coming down the
+# pike, and having the data in an array that can be easily accessed would be an
+# easy win.
+declare -a THEARRAY
+for l in $THESTRING; do
+       THEARRAY+=($l)
+done
+
+## Saved for later
+
+for k in "${!THEARRAY[@]}"
+do
+    echo $k ' - ' ${THEARRAY["$k"]}
+done
+# |
+# sort -rn -k3
