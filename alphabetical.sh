98c691b2d9fa2cdb2040835f85b4717475966464
Jeremy DeLong <jeremy@messageagency.com>
2018-02-15 13:04:46 -0500

    ready to submit

--- alphabetical.sh
+++ alphabetical.sh
@@ -19,48 +19,67 @@
 ##############
 
 # This is the string that is the whole point of the exercise
-FrontLineString="(id,created,employee(id,firstname,employeeType(id), lastname),location)"
+frontLineString="(id,created,employee(id,firstname,employeeType(id), lastname),location)"
 
 # Let's Declare some arrays...
-declare -a FrontLineArray
-declare -a FirstArray
-declare -a SecondArray
-declare -a ThirdArray
-declare -a ReassembledArray
+declare -a frontLineArray
+declare -a firstArray
+declare -a secondArray
+declare -a thirdArray
+declare -a reassembledArray
 
 # Step 1:  Let's split the string into space delimited pieces.
 #
-FrontLineString=$(echo ${FrontLineString} | sed 's/(/ /g')
-FrontLineString=$(echo ${FrontLineString} | sed 's/)/ /g')
-FrontLineString=$(echo ${FrontLineString} | sed 's/,/ /g')
+frontLineString=$(echo ${frontLineString} | sed 's/(/ /g')
+frontLineString=$(echo ${frontLineString} | sed 's/)/ /g')
+frontLineString=$(echo ${frontLineString} | sed 's/,/ /g')
 
 # Step 2.  Let's put the stringy bits into an array.
-for l in $FrontLineString; do
-    FrontLineArray+=(${l})
+for l in $frontLineString; do
+    frontLineArray+=(${l})
 done
 
 # TODO: These should be split into three diff arrays, that way they could each
-# be sorted separately, and then reassembled in correct order.
-for l in ${!FrontLineArray[@]}; do
-  if [ $l = 6 ]; then
-    ThirdArray+=(${FrontLineArray[$l]})
-  elif [ $l = 3 ] || [ $l = 4 ] || [ $l = 5 ] || [ $l = 7 ]; then
-    SecondArray+=(${FrontLineArray[$l]})
+# be sorted separately, and then reassembled in correct order. - DONE
+for each in ${!frontLineArray[@]}; do
+  if [ $each = 6 ]; then
+    thirdArray+=(" -- ${frontLineArray[$each]}")
+  elif [ $each = 3 ] || [ $each = 4 ] || [ $each = 5 ] || [ $each = 7 ]; then
+    secondArray+=(${frontLineArray[$each]})
   else
-    FirstArray+=(${FrontLineArray[$l]})
+    firstArray+=(${frontLineArray[$each]})
   fi
 done
 
-SecondArray=($(for each in ${SecondArray[@]}; do echo $each; done | sort))
-echo ${SecondArray[@]}
+# I want to output the arrays back into themselves in sorted order.  Don't need
+# to do this for the third, since that is an array of one.
+secondArray=($(for each in ${secondArray[@]}; do echo $each; done | sort))
+firstArray=($(for each in ${firstArray[@]}; do echo $each; done | sort))
 
-FirstArray=($(for each in ${FirstArray[@]}; do echo $each; done | sort))
-echo ${FirstArray[@]}
+# Now I need to add the - to the front of the secondArray elements. I did this
+# as a second step, since the sort was breaking on the '-'.
+for each in "${!secondArray[@]}"
+do
+  secondArray[${each}]=" - ${secondArray[each]}"
+done
 
+# @TODO: THERE HAS TO BE A BETTER WAY!  I have been struggling with this, but I
+# don't want the solution to take 3x more code than the problem.  I wanted to
+# merge the arrays, inserting the arrays in a way that would allow to pop, but I
+# couldn't get it to work the way I wanted it to.  This displays in alphabetical
+# order, but oy vay...
 
+reassembledArray[0]=${firstArray[0]}
+reassembledArray[1]=${firstArray[1]}
+reassembledArray[2]=${secondArray[0]}
+reassembledArray[3]=${thirdArray[0]}
+reassembledArray[4]=${secondArray[1]}
+reassembledArray[5]=${secondArray[2]}
+reassembledArray[6]=${secondArray[3]}
+reassembledArray[7]=${firstArray[2]}
+reassembledArray[8]=${firstArray[3]}
 
-######
-#This works, and gives the expected info.  It is ineligent though, and I should
-#have time tomorrow to clean it up before I have to turn it in.  I think that
-#creating three arrays is still the better way to do it, since then I can sort
-#them before reassembly.
+for each in "${!reassembledArray[@]}"
+do
+  echo ${reassembledArray["$each"]}
+done
--- frontine.sh
+++ frontine.sh
@@ -16,63 +16,48 @@
 # - lastname
 # location
 #
-# Bonus (output in alphabetical order):
-# created
-# employee
-# - employeeType
-# -- id
-# - firstname
-# - id
-# - lastname
-# id
-# location
 ##############
 
+# This is the string that is the whole point of the exercise
+frontLineString="(id,created,employee(id,firstname,employeeType(id), lastname),location)"
 
-THESTRING="(id,created,employee(id,firstname,employeeType(id), lastname),location)"
+declare -a frontLineArray
+declare -a reassembledArray
 
 # Step 1:  Let's split the string into space delimited pieces.
-#
-THESTRING=$(echo ${THESTRING} | sed 's/(/ /g')
-THESTRING=$(echo ${THESTRING} | sed 's/)/ /g')
-THESTRING=$(echo ${THESTRING} | sed 's/,/ /g')
+
+frontLineString=$(echo ${frontLineString} | sed 's/(/ /g')
+frontLineString=$(echo ${frontLineString} | sed 's/)/ /g')
+frontLineString=$(echo ${frontLineString} | sed 's/,/ /g')
 
 # Step 2.  Let's put the stringy bits into an array.  If I were teaching a jr.
 # dev, I would want to teach them to anticipate what might be coming down the
 # pike, and having the data in an array that can be easily accessed would be an
 # easy win.
-declare -a THEARRAY
-declare -a THENEWARRAY
 
-for l in $THESTRING; do
-    THEARRAY+=(${l})
+for l in $frontLineString; do
+    frontLineArray+=(${l})
 done
 
-# TODO: These should be split into three diff arrays, that way they could each
-# be sorted separately, and then reassembled in correct order.
-for l in ${!THEARRAY[@]}; do
+# Now we need to loop through and add the hyphens in the correct spots.
+
+for l in ${!frontLineArray[@]}; do
   if [ $l = 6 ]; then
-    THENEWARRAY+=(" -- ${THEARRAY[$l]}")
+    reassembledArray+=(" -- ${frontLineArray[$l]}")
   elif [ $l = 3 ] || [ $l = 4 ] || [ $l = 5 ] || [ $l = 7 ]; then
-    THENEWARRAY+=(" - ${THEARRAY[$l]}")
+    reassembledArray+=(" - ${frontLineArray[$l]}")
   else
-    THENEWARRAY+=(${THEARRAY[$l]})
+    reassembledArray+=(${frontLineArray[$l]})
   fi
 done
 
-
-for k in "${!THENEWARRAY[@]}"
+for k in "${!reassembledArray[@]}"
 do
-    echo ${THENEWARRAY["$k"]}
+    echo ${reassembledArray["$k"]}
 done
 
 ######
-#This works, and gives the expected info.  It is ineligent though, and I should
-#have time tomorrow to clean it up before I have to turn it in.  I think that
-#creating three arrays is still the better way to do it, since then I can sort
-#them before reassembly.
-
-
-
-# This would be how I could handle the sort tomorrow...
-#| sort -k3
+# This works, and gives the expected info.  It is ineligent though, and I should
+# have time tomorrow to clean it up before I have to turn it in.  I think that
+# creating three arrays is still the better way to do it, since then I can sort
+# them before reassembly.
